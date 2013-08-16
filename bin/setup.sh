f41c611fbb9fb0b5c39756b6474f4b16b0a62fb6
Noah Hoffman <noah.hoffman@gmail.com>
2013-08-15 17:11:56 -0700

    - bootstrap virtualenv installation - use pip --relocatable to prevent overly long shebang lines

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,6 +3,9 @@
 # Create virtualenv, install scons from source, and install
 # requirements in requirements.txt
 
+# override the default python interpreter using
+# `PYTHON=/path/to/python bin/setup.sh`
+
 set -e
 
 if [[ -z $1 ]]; then
@@ -11,18 +14,37 @@ else
     venv=$1
 fi
 
+mkdir -p src
+
+# bootstrap a recent version of virtualenv; inspired by
+# http://eli.thegreenplace.net/2013/04/20/bootstrapping-virtualenv/
+VENV_VERSION=1.10.1
+PYPI_VENV_BASE='http://pypi.python.org/packages/source/v/virtualenv'
+if [[ -z $PYTHON ]]; then
+    PYTHON=$(which python)
+fi
+
+# download virtualenv source if necessary
+if [ ! -f src/virtualenv-${VENV_VERSION}/virtualenv.py ]; then
+    (cd src && \
+	wget -N ${PYPI_VENV_BASE}/virtualenv-${VENV_VERSION}.tar.gz && \
+	tar -xf virtualenv-${VENV_VERSION}.tar.gz)
+fi
+
+# create virtualenv if necessary
 if [ ! -f $venv/bin/activate ]; then
-    virtualenv $venv
+    $PYTHON src/virtualenv-${VENV_VERSION}/virtualenv.py $venv
+    $PYTHON src/virtualenv-${VENV_VERSION}/virtualenv.py --relocatable $venv
 else
     echo "found existing virtualenv $venv"
 fi
 
 source $venv/bin/activate
 
-mkdir -p src
+# scons can't be installed using pip
 if [ ! -f $venv/bin/scons ]; then
     (cd src && \
-	wget http://downloads.sourceforge.net/project/scons/scons/2.3.0/scons-2.3.0.tar.gz && \
+	wget -N http://downloads.sourceforge.net/project/scons/scons/2.3.0/scons-2.3.0.tar.gz && \
 	tar -xf scons-2.3.0.tar.gz && \
 	cd scons-2.3.0 && \
 	python setup.py install
@@ -31,4 +53,5 @@ else
     echo "scons is already installed in $(which scons)"
 fi
 
+# install other requirements
 pip install -r requirements.txt
