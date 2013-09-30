bd62d9098866beaf17778ae741c9756a78ced8d3
Noah Hoffman <noah.hoffman@gmail.com>
2013-09-30 14:30:07 -0700

    - rename script that bootstraps virtualenv to bootstrap.sh - bin/setup.sh creates virtualenv from local resources

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,59 +1,27 @@
 #!/bin/bash
 
-# Create virtualenv, install scons from source, and install
-# requirements in requirements.txt
-
-# override the default python interpreter using
-# `PYTHON=/path/to/python bin/setup.sh`
+# Create a virtualenv using local resources.
 
 set -e
 
-if [[ -z $1 ]]; then
-    venv=$(basename $(pwd))-env
-else
-    venv=$1
-fi
-
-mkdir -p src
-
-# bootstrap a recent version of virtualenv; inspired by
-# http://eli.thegreenplace.net/2013/04/20/bootstrapping-virtualenv/
-VENV_VERSION=1.10.1
-PYPI_VENV_BASE='http://pypi.python.org/packages/source/v/virtualenv'
-if [[ -z $PYTHON ]]; then
-    PYTHON=$(which python)
-fi
-
-# download virtualenv source if necessary
-if [ ! -f src/virtualenv-${VENV_VERSION}/virtualenv.py ]; then
-    (cd src && \
-	wget -N ${PYPI_VENV_BASE}/virtualenv-${VENV_VERSION}.tar.gz && \
-	tar -xf virtualenv-${VENV_VERSION}.tar.gz)
-fi
+BIN=~/local/bin
+venv=$(basename $(pwd))-env
 
 # create virtualenv if necessary
 if [ ! -f $venv/bin/activate ]; then
-    $PYTHON src/virtualenv-${VENV_VERSION}/virtualenv.py $venv
-    $PYTHON src/virtualenv-${VENV_VERSION}/virtualenv.py --relocatable $venv
+    $BIN/virtualenv -p $BIN/python --system-site-packages $venv
+    $BIN/virtualenv --relocatable $venv
 else
     echo "found existing virtualenv $venv"
 fi
 
+# make scons available
+ln -sf $BIN/scons $venv/bin
+
 source $venv/bin/activate
 
-# scons can't be installed using pip
-if [ ! -f $venv/bin/scons ]; then
-    (cd src && \
-	wget -N http://downloads.sourceforge.net/project/scons/scons/2.3.0/scons-2.3.0.tar.gz && \
-	tar -xf scons-2.3.0.tar.gz && \
-	cd scons-2.3.0 && \
-	python setup.py install
-    )
-else
-    echo "scons is already installed in $(which scons)"
-fi
-
 # install other requirements
-pip install -r requirements.txt
+$BIN/pip install -r requirements.txt
+
 # correct any more shebang lines
-$PYTHON src/virtualenv-${VENV_VERSION}/virtualenv.py --relocatable $venv
+$BIN/virtualenv --relocatable $venv
